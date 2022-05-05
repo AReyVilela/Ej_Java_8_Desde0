0099ce0da2d8b0888403f56f5788b8ca10fe728a
Alberto Rey Vilela <arey@stratio.com>
2022-05-05 14:40:39 +0200

    Supermercado Java 8

--- src/main/java/clases/Main.java
+++ src/main/java/clases/Main.java
@@ -2,26 +2,21 @@ package clases;
 
 import Excepciones.ErrorFormatIntroducido;
 import Excepciones.ListaProductosNegativa;
-import Interfaces.IProducto;
+import Excepciones.NoExisteProducto;
 import Interfaces.IReponedor;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
+import java.io.IOException;
 import java.util.Scanner;
 
-import static java.util.stream.Collectors.groupingBy;
-
 public class Main {
-    public static void main(String[] args) throws ErrorFormatIntroducido {
-
-        //Inicializo la opcion del menu y leo el teclado
+    public static void main(String[] args) throws ErrorFormatIntroducido, IOException, NoExisteProducto {
         int opcion = 0;
         Scanner sc = new Scanner(System.in);
 
-        //Creo una instancia de supermercado y llamo a su hasmap apra rellenar
         Supermercado supermercado = new Supermercado();
-        IReponedor.reponerSupermercado(supermercado.productos, supermercado.stock, supermercado.mapStockProducto);
+        IReponedor.reponerSupermercado(supermercado);
+
+        //LLAMO A REPONER LA PRIMERA VEZ CON EL CSV
 
         try {
             do {
@@ -40,17 +35,33 @@ public class Main {
                     case 2://MOSTRAR CAJA
                         supermercado.mostrarValorCaja();
                         break;
-                    case 3://SALIR
+                    case 3:
+                        //filtrar por stock
+                        System.out.println("Indique el stock maximo: ");
+                        int stockFilter = Integer.parseInt(sc.nextLine());
+                        supermercado.filtrarProductosporStock(stockFilter);
+                        break;
+                    case 4:
+                        //ordenados por precio
+                        supermercado.ordenarPorPrecio();
+                        break;
+                    case 5:
+                        //ordenados por stock
+                        supermercado.ordendarPorStock();
+                        break;
+                    case 6://SALIR
                         System.out.println("SALIENDO...GRACIAS !!");
                         break;
                     default:
                         System.out.println("Opcion no válida ");
                         System.out.println(" ");
                 }
-            } while (opcion != 3);
+            } while (opcion != 6);
         } catch (NumberFormatException e) {
             System.out.println("Error de formato, se pide un número -->  " + e);
         } catch (ListaProductosNegativa e) {
+            throw new RuntimeException(e);
+        } catch (NoExisteProducto e) {
             throw new RuntimeException(e);
         }
         sc.close();
@@ -63,7 +74,10 @@ public class Main {
         System.out.println("0. Listar Productos");
         System.out.println("1. Vender Producto por id");
         System.out.println("2. Mostrar caja");
-        System.out.println("3. Salir");
+        System.out.println("3. Listar productos con stock menor a X ");
+        System.out.println("4. Listar productos ordenados por precio");
+        System.out.println("5. Listar productos ordenados por stock");
+        System.out.println("6. Salir");
         System.out.println("Elige una opcion: ");
     }
 }
